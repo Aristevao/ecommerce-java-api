838ed2ddee782b565bd829c622f441a57a8b7379
Aristevao <aristevao@hotmail.com>
2022-02-07 12:16:17 -0300

    [M-00] Implement hexagonal architecture package structure

--- src/main/java/com/mentoring/ecommerce/EcommerceApplication.java
+++ src/main/java/com/mentoring/ecommerce/EcommerceApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication { // TODO | Check the need to implement the CommandLineRunner interface, as done in the dummy project
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
-
 }
