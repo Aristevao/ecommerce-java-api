5a9d81a9c377f62a7e390e0d795539e960a6d03d
Aristevao <aristevao@hotmail.com>
2022-05-18 09:23:15 -0300

    [M-12] Change error handling

--- src/main/java/com/mentoring/ecommerce/adapter/in/web/ProductControllerValidAdvice.java
+++ src/main/java/com/mentoring/ecommerce/adapter/in/web/ProductControllerValidAdvice.java
@@ -4,19 +4,38 @@ import com.mentoring.ecommerce.adapter.in.web.response.ErrorEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ControllerAdvice
 public class ProductControllerValidAdvice extends ResponseEntityExceptionHandler {
-    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String message = exception.getBindingResult().getFieldError().getDefaultMessage();
-        final ErrorEntity error1 = new ErrorEntity(BAD_REQUEST.value(), message, message, ((ServletWebRequest) request).getRequest().getRequestURI());
-        return new ResponseEntity<>(error1, BAD_REQUEST);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        String message = exception.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        for (FieldError error : exception.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        for (ObjectError error : exception.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+        }
+
+        final ErrorEntity error = new ErrorEntity(BAD_REQUEST.value(), errors, message, ((ServletWebRequest) request).getRequest().getRequestURI());
+        return handleExceptionInternal(
+                exception, error, headers, BAD_REQUEST, request);
     }
 }
--- src/main/java/com/mentoring/ecommerce/adapter/in/web/response/ErrorEntity.java
+++ src/main/java/com/mentoring/ecommerce/adapter/in/web/response/ErrorEntity.java
@@ -3,19 +3,29 @@ package com.mentoring.ecommerce.adapter.in.web.response;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @Data
 public class ErrorEntity {
     private LocalDateTime timestamp;
     private int status;
-    private String error;
+    private List<String> errors;
     private String message;
     private String path;
 
     public ErrorEntity(final int status, final String error, final String message, final String path) {
         this.timestamp = LocalDateTime.now();
         this.status = status;
-        this.error = error;
+        errors = List.of(error);
+        this.message = message;
+        this.path = path;
+    }
+
+    public ErrorEntity(final int status, final List<String> errors, final String message, final String path) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.errors = errors;
         this.message = message;
         this.path = path;
     }
